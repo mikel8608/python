30cb47f609947084ff391d2244de4bf12cdfeb90
Michael Lean <michael.lean@ticketek.com.au>
2021-02-13 23:28:50 +1100

    updates

--- vars_lists.py
+++ vars_lists.py
@@ -7,6 +7,7 @@ import random
 print('\n*** Lists are indexed collections of non-unique elements ***\n')
 
 data = [1,2,'A','B',1]
+data_ints = [1,2,6,2,5,4]
 
 def show_data():
   print('Showing Data')
@@ -28,8 +29,12 @@ def insert_data():
   show_data()
 
 def delete_data():
-  print('\nDelete 1st element')
+  print("\nDelete 1st element using 'del'")
   del data[0]
+  show_data()
+
+  print("\nDelete element with 'pop()'",'If no index is provided it pops off the last element')
+  data.pop(2)
   show_data()
 
 def concatenate_data():
@@ -37,16 +42,29 @@ def concatenate_data():
   data2 = ['data2','data3']
   print(data+data2)
   show_data()
-    
+
+def sort_reverse_data():
+  print("\nSorted")
+  print(sorted(data_ints)) #py3, #py2 = print(data_ints.sort()) 
+  
+  data_ints.reverse()
+  print(data_ints) # This doesn't work: print(data_ints.reverse())
+
+  list1 = ['physics', 'Biology', 'chemistry', 'maths']
+  list1.reverse()
+  print ("list now : ", list1)
+
+
 # Run Functions
 print('data: ',data)
 print("data, Show every 2nd entry: ",data[::2]) #see strings.py for more inof on [:::]
 print('random value selected from data:',random.choice(data),'\n')
 
-concatenate_data()
 #show_data()
+#concatenate_data()
 #append_data()
 #insert_data()
 #delete_data()
+sort_reverse_data()
 
 print('\n*** Lists are indexed collections of non-unique elements ***\n')
